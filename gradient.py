21baff6a944909a235b9e4f4cfe94b3bc81876d5
Jonatan Kasperczak <jakasperczak@gmail.com>
2025-03-16 18:39:08 +0100

    funkcja kosztu2

--- gradient.py
+++ gradient.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
-import matplotlib as mpt
+import matplotlib as plt
 
-print(dir(np))
+
+# funkcja kwadratowa
+# ax^2+bx+c
+# d/dx 2ax+b
+
+# funkcja kosztu -> suma kwadratów różnicy pomiędzy właściwą a przewidywaną wartością 
+
+def cost_function(true, predicted):
+    cost = np.sum((true - predicted)**2) / len (true)
+    return cost
+
